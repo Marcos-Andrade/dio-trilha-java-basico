0b223d879b9b6c8115c67b4cb1d14e8c8ddda733
Marcos-Andrade <andrademarcosferreira3@gmail.com>
2025-04-25 17:59:50 -0300

    Inciando em condicionais - if ternário

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,6 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         
+        // Exemplo de estrutura condicional
+        /*
         int nota = 6;
 
         if(nota >= 7) {
@@ -14,5 +16,12 @@ public class ResultadoEscolar {
         else {
             System.out.println("Reprovado!");
         }
+        */
+
+        // Exemplo de estrutura de condicional ternária
+        int nota = 6;  
+        String resultado = (nota >= 7) ? "Aprovado!" : (nota >= 5 && nota < 7) ? "Recuperação!" : "Reprovado!";
+
+        System.err.println(resultado);
     }
 }
