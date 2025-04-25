bda4de3760eeb439346bfe34b6d6feeb8cec1dc8
Marcos-Andrade <andrademarcosferreira3@gmail.com>
2025-04-25 17:34:16 -0300

    Inciando em condicionais - condicional encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,17 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         
-        int nota = 9;
+        int nota = 6;
 
         if(nota >= 7) {
             System.out.println("Aprovado!");
-        } else {
+        } 
+        
+        else if(nota >= 5 && nota < 7) {
+            System.out.println("Recuperação!");
+        } 
+        
+        else {
             System.out.println("Reprovado!");
         }
     }
